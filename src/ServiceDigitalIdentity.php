029dd3789b0830525dbcf8877d98742e80d91f18
Liam Dennehy <liam@wiemax.net>
2018-11-09 00:00:41 +0100

    ServiceDigitalIdentity: Registry of DigitalIDs for each TL

--- src/ServiceDigitalIdentity.php
+++ src/ServiceDigitalIdentity.php
@@ -7,19 +7,37 @@ namespace eIDASCertificate;
  */
 class ServiceDigitalIdentity
 {
-  private $x509Certificate;
+    private $x509Certificates = [];
 
-  public function __construct($digitalIdentity) {
-    $this->x509Certificate = openssl_x509_read(
-      $this->string2pem(
-        (string)$digitalIdentity->DigitalId->X509Certificate
-      )
-    );
-  }
+    public function __construct($serviceDigitalIdentities)
+    {
+        foreach ($serviceDigitalIdentities->xpath('*') as $serviceDigitalIdentity) {
+            $this->x509Certificates[] = openssl_x509_read(
+        $this->string2pem(
+          (string)$serviceDigitalIdentity->DigitalId->X509Certificate
+        )
+      );
+        };
+    }
 
-  private function string2pem($certificateString) {
-    return "-----BEGIN CERTIFICATE-----\n" .
+    private function string2pem($certificateString)
+    {
+        return "-----BEGIN CERTIFICATE-----\n" .
       chunk_split($certificateString, 64, "\n") .
       "-----END CERTIFICATE-----\n";
-  }
+    }
+
+    public function getX509Certificates()
+    {
+        return $this->x509Certificates;
+    }
+
+    public function getX509Thumbprints()
+    {
+        $thumbprints = [];
+        foreach ($this->x509Certificates as $x509Certificate) {
+            $thumbprints[] = openssl_x509_fingerprint($x509Certificate);
+        };
+        return $thumbprints;
+    }
 }
