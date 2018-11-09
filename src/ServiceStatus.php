7c1e5a34683e38ad550972f47d542025b50cbca0
Liam Dennehy <liam@wiemax.net>
2018-11-09 02:15:26 +0100

    ServiceStatus

---
+++ src/ServiceStatus.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace eIDASCertificate;
+
+/**
+ *
+ */
+class ServiceStatus
+{
+
+    private $status;
+    public function __construct($status)
+    {
+        switch ($status) {
+          case 'http://uri.etsi.org/TrstSvc/TrustedList/Svcstatus/deprecatedatnationallevel':
+            $this->status = 'deprecatedatnationallevel';
+            break;
+          case 'http://uri.etsi.org/TrstSvc/TrustedList/Svcstatus/granted':
+            $this->status = 'granted';
+            break;
+          case 'http://uri.etsi.org/TrstSvc/TrustedList/Svcstatus/recognisedatnationallevel':
+            $this->status = 'recognisedatnationallevel';
+            break;
+          case 'http://uri.etsi.org/TrstSvc/TrustedList/Svcstatus/withdrawn':
+            $this->status = 'withdrawn';
+            break;
+          default:
+            throw new \Exception("Unknown Service Status Identifier '$status'", 1);
+
+            break;
+        }
+    }
+}
