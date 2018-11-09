aef2f9afe5ea8d301016a390ee65c1d6bee5d031
Liam Dennehy <liam@wiemax.net>
2018-11-09 23:49:59 +0100

    ServiceStatus: Expanded to include ServiceHistory enumerations

--- src/ServiceStatus.php
+++ src/ServiceStatus.php
@@ -8,9 +8,21 @@ namespace eIDASCertificate;
 class ServiceStatus
 {
     private $status;
+    private $uri;
+
     public function __construct($status)
     {
+        $this->uri = $status;
         switch ($status) {
+          case 'http://uri.etsi.org/TrstSvc/TrustedList/Svcstatus/accreditationceased':
+            $this->status = 'accreditationceased';
+            break;
+          case 'http://uri.etsi.org/TrstSvc/TrustedList/Svcstatus/accreditationrevoked':
+            $this->status = 'accreditationrevoked';
+            break;
+          case 'http://uri.etsi.org/TrstSvc/TrustedList/Svcstatus/accredited':
+            $this->status = 'accredited';
+            break;
           case 'http://uri.etsi.org/TrstSvc/TrustedList/Svcstatus/deprecatedatnationallevel':
             $this->status = 'deprecatedatnationallevel';
             break;
@@ -20,12 +32,27 @@ class ServiceStatus
           case 'http://uri.etsi.org/TrstSvc/TrustedList/Svcstatus/recognisedatnationallevel':
             $this->status = 'recognisedatnationallevel';
             break;
+          case 'http://uri.etsi.org/TrstSvc/TrustedList/Svcstatus/setbynationallaw':
+            $this->status = 'setbynationallaw';
+            break;
+          case 'http://uri.etsi.org/TrstSvc/TrustedList/Svcstatus/supervisionceased':
+            $this->status = 'supervisionceased';
+            break;
+          case 'http://uri.etsi.org/TrstSvc/TrustedList/Svcstatus/supervisionincessation':
+            $this->status = 'supervisionincessation';
+            break;
+          case 'http://uri.etsi.org/TrstSvc/TrustedList/Svcstatus/supervisionrevoked':
+            $this->status = 'supervisionrevoked';
+            break;
+          case 'http://uri.etsi.org/TrstSvc/TrustedList/Svcstatus/undersupervision':
+            $this->status = 'undersupervision';
+            break;
           case 'http://uri.etsi.org/TrstSvc/TrustedList/Svcstatus/withdrawn':
             $this->status = 'withdrawn';
             break;
           default:
+            $this->uri = null;
             throw new ParseException("Unknown Service Status Identifier '$status'", 1);
-
             break;
         }
     }
@@ -34,4 +61,10 @@ class ServiceStatus
     {
         return $this->status;
     }
+
+    public function getURI()
+    {
+        return $this->uri;
+    }
+
 }
