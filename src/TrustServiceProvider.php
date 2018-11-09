8008a42e8c09d7987acd637ac65895256665012b
Liam Dennehy <liam@wiemax.net>
2018-11-09 12:10:08 +0100

    TrustServiceProvider: Indexing and getters

--- src/TrustServiceProvider.php
+++ src/TrustServiceProvider.php
@@ -10,14 +10,33 @@ class TrustServiceProvider
     private $name;
     private $services = [];
     private $serviceHistory;
+    private $verbose;
+
     public function __construct($tsp)
     {
-        $this->name = $tsp->TSPInformation->TSPName->xpath("*[@xml:lang='en']")[0];
+        // $this->verbose = $verbose;
+        $this->name = (string)$tsp->TSPInformation->TSPName->xpath("*[@xml:lang='en']")[0];
         foreach ($tsp->TSPServices->TSPService as $tspService) {
-            $services[] = new TSPService($tspService);
-        }
+            $newTSPService = new TSPService($tspService);
+            $this->services[$newTSPService->getDate()] = $newTSPService;
+        };
+        sort($this->services);
         $this->serviceHistory = new ServiceHistory(
           $tspService->ServiceHistory
         );
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function getServices()
+    {
+        $services = [];
+        foreach ($this->services as $service) {
+            $services[] = $service->getService();
+        };
+        return $services;
     }
 }
