b2c7263bc0becbddac096d71ad1623dfc86e769e
Liam Dennehy <liam@wiemax.net>
2018-11-09 12:10:32 +0100

    TSPService: Status date and getters

--- src/TSPService.php
+++ src/TSPService.php
@@ -7,15 +7,33 @@ namespace eIDASCertificate;
  */
 class TSPService
 {
-    private $identifier;
+    private $serviceType;
     private $status;
+    private $startingTime;
+
     public function __construct($tspService)
     {
-        $this->identifier = new ServiceTypeIdentifier(
+        $this->serviceType = new ServiceType(
             (string)$tspService->ServiceInformation->ServiceTypeIdentifier
         );
         $this->status = new ServiceStatus(
-            (string)$tspService->ServiceInformation->ServiceStatus    
+            (string)$tspService->ServiceInformation->ServiceStatus
         );
+        $this->startingTime = strtotime((string)$tspService->ServiceInformation->StatusStartingTime);
+    }
+
+    public function getService()
+    {
+        return [
+          "type" => $this->serviceType->getType(),
+          "isQualified" => $this->serviceType->isQualified(),
+          "status" => $this->status->getStatus(),
+          "startingTime" => $this->startingTime
+        ];
+    }
+
+    public function getDate()
+    {
+        return $this->startingTime;
     }
 }
