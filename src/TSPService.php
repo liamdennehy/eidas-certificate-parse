1f3af18d57a94c772651799c20092a2d98763fd5
Liam Dennehy <liam@wiemax.net>
2018-11-10 16:18:12 +0100

    Bugfix: Properly reference object property

--- src/TSPService.php
+++ src/TSPService.php
@@ -26,13 +26,9 @@ class TSPService
         );
         $this->startingTime = strtotime((string)$serviceInformation->StatusStartingTime);
         foreach ($serviceInformation->ServiceDigitalIdentity->DigitalId as $identity) {
-            $identities[] = new ServiceDigitalIdentity($identity);
+            $this->identities[] = new ServiceDigitalIdentity($identity);
         };
-        // var_dump(count($serviceInformation->ServiceInformationExtensions));
         if (count($serviceInformation->ServiceInformationExtensions)) {
-            // $stdin = fopen('php://stdin', 'r');
-            // $response = fgets($stdin);
-            // var_dump($serviceInformation->ServiceInformationExtensions->Extension);
             foreach ($serviceInformation->ServiceInformationExtensions->Extension as $siExtension) {
                 // Apparently https://stackoverflow.com/questions/27742595/php-best-way-to-stop-constructor
                 try {
@@ -40,12 +36,12 @@ class TSPService
                     if ($verbose) {
                         print '        ' . $newSIExtension->getURI() . PHP_EOL;
                     };
-                    $siExtensions[] = $newSIExtension;
+                    $this->siExtensions[] = $newSIExtension;
                 } catch (SafeException $e) {
                     continue;
                 }
             }
-        }
+        };
     }
 
     public function getService()
@@ -55,7 +51,8 @@ class TSPService
           "isQualified" => $this->serviceType->isQualified(),
           "status" => $this->status->getStatus(),
           "startingTime" => $this->startingTime,
-          "identities" => $this->identities
+          "identities" => $this->identities,
+          "siExtensions" => $this->siExtensions
         ];
     }
 
