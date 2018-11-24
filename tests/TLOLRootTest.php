6d4ecc23ceea5160ff69a24b40fb41b96dd7daab
Liam Dennehy <liam@wiemax.net>
2018-11-24 16:54:50 +0100

    TLOLRootTests

--- tests/TLOLRootTest.php
+++ tests/TLOLRootTest.php
@@ -9,11 +9,13 @@ use eIDASCertificate\TrustedList;
 class TLOLRootTest extends TestCase
 {
     private $tlolxml;
+    private $tlol;
     public function setUp()
     {
         if (! $this->tlolxml) {
             $this->tlolxml = DataSource::fetch(
-                TrustedList::TrustedListOfListsXMLPath);
+                TrustedList::TrustedListOfListsXMLPath
+            );
         }
     }
 
@@ -36,19 +38,52 @@ class TLOLRootTest extends TestCase
             ],
             openssl_x509_parse($TrustedListOfLists->getSignedBy())['subject']
         );
+        $this->tlol = $TrustedListOfLists;
     }
 
-    public function testVerifyAllTLs()
+    public function testGetTrustedListPointers()
     {
-        $TrustedListOfLists = new TrustedList($this->tlolxml, null, false);
-        $TrustedListOfLists->verifyTSL();
-        $this->assertTrue($TrustedListOfLists->verifyAllTLs(true));
+        if (! $this->tlol) {
+            $TrustedListOfLists = new TrustedList($this->tlolxml, null, false);
+            $TrustedListOfLists->verifyTSL();
+            $this->tlol = $TrustedListOfLists;
+        };
+        $this->assertGreaterThan(
+            0,
+            sizeof($this->tlol->getTrustedListPointers())
+        );
+    }
+
+    public function testGetTLOLTrustedLists()
+    {
+        if (! $this->tlol) {
+            $TrustedListOfLists = new TrustedList($this->tlolxml, null, false);
+            $TrustedListOfLists->verifyTSL();
+            $this->tlol = $TrustedListOfLists;
+        };
+        // $this->tlol->verifyAllTLs();
+        $tls = $this->tlol->getTrustedLists();
+        $this->assertGreaterThan(
+            0,
+            sizeof($tls)
+        );
+    }
+
+    public function testAllTLsVerified()
+    {
+        if (! $this->tlol) {
+            $TrustedListOfLists = new TrustedList($this->tlolxml, null, false);
+            $TrustedListOfLists->verifyTSL();
+            $this->tlol = $TrustedListOfLists;
+        };
+        $tls = $this->tlol->getTrustedLists();
         $failedTLVerify = false;
-        foreach ($TrustedListOfLists as $tl) {
-            if (! $tl->getSignedBy) {
+        foreach ($tls as $tl) {
+            if (! $tl->getSignedBy()) {
                 $failedTLVerify = true;
             }
         };
         $this->assertFalse($failedTLVerify);
+        $this->assertTrue($this->tlol->verifyAllTLs());
     }
 }
