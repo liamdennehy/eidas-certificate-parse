c78e44fd74ee3b39f74185102d51ad148c2b1e59
Liam Dennehy <liam@wiemax.net>
2019-09-08 14:18:37 +0200

    LOTL: Explicit Signed tests

--- tests/LOTLRootTest.php
+++ tests/LOTLRootTest.php
@@ -10,23 +10,29 @@ use eIDASCertificate\Certificate\X509Certificate;
 
 class LOTLRootTest extends TestCase
 {
+    const lotlXMLFileName = 'eu-lotl-248.xml';
+
     private $lotlxml;
     private $lotl;
+    private $datadir;
 
     public function setUp()
     {
-        if (! $this->lotlxml) {
-            $this->lotlxml = DataSource::load(
+        $this->datadir = __DIR__ . '/../data';
+        $xmlFilePath = $this->datadir.self::lotlXMLFileName;
+        if (! file_exists($xmlFilePath)) {
+            $this->lotlXML = DataSource::getHTTP(
                 TrustedList::ListOfTrustedListsXMLPath
             );
-        };
-        if (! $this->lotl) {
-            $this->lotl = new TrustedList($this->lotlxml, null, false);
-        };
+            file_put_contents($xmlFilePath, $this->lotlXML);
+        } else {
+            $this->lotlXML = file_get_contents($xmlFilePath);
+        }
     }
 
-    public function testLOTLAttributes()
+    public function testParseLOTL()
     {
+        $this->lotl = new TrustedList($this->lotlXML);
         $this->assertEquals(
             "EUlistofthelists",
             $this->lotl->getTSLType()->getType()
@@ -41,10 +47,7 @@ class LOTLRootTest extends TestCase
             1,
             $this->lotl->getSequenceNumber()
         );
-    }
 
-    public function testLOTLCertificates()
-    {
         $this->assertGreaterThan(
             0,
             sizeof($this->lotl->getTLX509Certificates())
@@ -57,38 +60,85 @@ class LOTLRootTest extends TestCase
         }
     }
 
-    public function testVerifyLOTL()
+    public function testVerifyLOTLSelfSigned()
     {
-        // $expectedSignedByDNArray =
-        // [
-        //     'C' => 'NL',
-        //     'L' => 'BE',
-        //     'O' => 'European Commission',
-        //     'OU' => '0949.383.342',
-        //     'CN' => 'Michael Theodoor de Boer',
-        //     'SN' => 'de Boer',
-        //     'GN' => 'Michael Theodoor',
-        //     'serialNumber' => '10303969450085046424',
-        //     'emailAddress' => 'michael.de-boer@ec.europa.eu',
-        //     'title' => 'Professional Person'
-        // ];
-        $this->assertTrue($this->lotl->verifyTSL());
-        // $lotlSignedByCert = $this->lotl->getSignedBy();
-        // $lotlSignedByDNArray = openssl_x509_parse($lotlSignedByCert)['subject'];
-        // $this->assertEquals(
-        //     $expectedSignedByDNArray,
-        //     $lotlSignedByDNArray
-        // );
+        $lotl = new TrustedList($this->lotlXML);
+        $this->assertTrue($lotl->verifyTSL());
+        $expectedSignedByDNArray =
+        [
+          'C' => 'BE',
+          'CN' => 'Patrick Kremer (Signature)',
+          'SN' => 'Kremer',
+          'GN' => 'Patrick Jean',
+          'serialNumber' => '72020329970',
+        ];
+        $lotlSignedByCert = $lotl->getSignedBy();
+        $lotlSignedByDNArray = openssl_x509_parse($lotlSignedByCert)['subject'];
+        $this->assertEquals(
+            $expectedSignedByDNArray,
+            $lotlSignedByDNArray
+        );
+    }
+
+    public function testVerifyLOTLExplicitSigned()
+    {
+        $wrongCertHash = '9c1a3b646eaf132398ef319e41c8e7ed725b64d5772580ae125d59c0f6845630';
+        $rightCertHash = 'd2064fdd70f6982dcc516b86d9d5c56aea939417c624b2e478c0b29de54f8474';
+        $certpaths = scandir($this->datadir.'/journal/c-276-1');
+        while ($certpaths[0] == '.' || $certpaths[0] == '..') {
+            array_shift($certpaths);
+        }
+        $certs = [];
+        foreach ($certpaths as $certpath) {
+            $id = explode('.', $certpath)[0];
+            $certs[$id] = file_get_contents($this->datadir.'/journal/c-276-1/'.$certpath);
+        }
+        $wrongCert = $certs[$wrongCertHash];
+        $rightCert = $certs[$rightCertHash];
+        $lotl = new TrustedList($this->lotlXML);
+        try {
+            $lotl->verifyTSL([$wrongCert]);
+            $this->assetTrue(false); // Should never hit this if exception is raised
+        } catch (\eIDASCertificate\Signature\SignatureException $e) {
+            $this->assertEquals(
+                [
+                'signedBy' => 'd2064fdd70f6982dcc516b86d9d5c56aea939417c624b2e478c0b29de54f8474',
+                'availableCerts' => [
+                  '9c1a3b646eaf132398ef319e41c8e7ed725b64d5772580ae125d59c0f6845630'
+                ]
+              ],
+                $e->getOut()
+            );
+        }
+        $lotl = new TrustedList($this->lotlXML);
+        $this->assertTrue($lotl->verifyTSL([$wrongCert,$rightCert]));
+        $lotl = new TrustedList($this->lotlXML);
+        $this->assertTrue($lotl->verifyTSL($rightCert));
+        $expectedSignedByDNArray =
+        [
+          'C' => 'BE',
+          'CN' => 'Patrick Kremer (Signature)',
+          'SN' => 'Kremer',
+          'GN' => 'Patrick Jean',
+          'serialNumber' => '72020329970',
+        ];
+        $lotlSignedByCert = $lotl->getSignedBy();
+        $lotlSignedByDNArray = openssl_x509_parse($lotlSignedByCert)['subject'];
+        $this->assertEquals(
+            $expectedSignedByDNArray,
+            $lotlSignedByDNArray
+        );
     }
 
     public function testGetLOTLTrustedListXMLPointers()
     {
+        $lotl = new TrustedList($this->lotlXML);
         $validURLFilterFlags =
             FILTER_FLAG_PATH_REQUIRED;
         // FILTER_FLAG_PATH_REQUIRED |
         // FILTER_FLAG_HOST_REQUIRED |
         // FILTER_FLAG_SCHEME_REQUIRED;
-        $tlXMLPointers = $this->lotl->getTrustedListPointers('xml');
+        $tlXMLPointers = $lotl->getTrustedListPointers('xml');
         $this->assertGreaterThan(
             12,
             sizeof($tlXMLPointers)
