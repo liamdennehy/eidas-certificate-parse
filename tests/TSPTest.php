c49614958ed73516b6ef20b32f432cf787704850
Liam Dennehy <liam@wiemax.net>
2019-09-09 09:23:06 +0200

    TSPTest: Localise storage

--- tests/TSPTest.php
+++ tests/TSPTest.php
@@ -9,39 +9,53 @@ use eIDASCertificate\Certificate\X509Certificate;
 
 class TSPTest extends TestCase
 {
-    private $lotlxml;
+    const lotlXMLFileName = 'eu-lotl.xml';
+
+    private $lotlXML;
     private $lotl;
     private $tls;
     private $dataSource;
+    private $dataDir;
 
     public function setUp()
     {
-        if (! $this->dataSource) {
-            $this->dataSource = new DataSource("sqlite", "/data");
-        }
-        if (! $this->lotlxml) {
-            $this->lotlxml = $this->dataSource->load(TrustedList::ListOfTrustedListsXMLPath, 'trustedLists');
-        }
-        if (! $this->lotl) {
-            $this->lotl = new TrustedList(
-                file_get_contents(__DIR__ . '/../data/eu-lotl.xml')
+        $this->datadir = __DIR__ . '/../data';
+        $xmlFilePath = $this->datadir.self::lotlXMLFileName;
+        if (! file_exists($xmlFilePath)) {
+            $this->lotlXML = DataSource::getHTTP(
+                TrustedList::ListOfTrustedListsXMLPath
             );
-        };
+            file_put_contents($xmlFilePath, $this->lotlXML);
+        } else {
+            $this->lotlXML = file_get_contents($xmlFilePath);
+        }
+
+        // if (! $this->dataSource) {
+        //     $this->dataSource = new DataSource("sqlite", "/data");
+        // }
+        // if (! $this->lotlxml) {
+        //     $this->lotlxml = $this->dataSource->load(TrustedList::ListOfTrustedListsXMLPath, 'trustedLists');
+        // }
+        // if (! $this->lotl) {
+        //     $this->lotl = new TrustedList(
+        //         file_get_contents(__DIR__ . '/../data/eu-lotl.xml')
+        //     );
+        // };
     }
 
-    public function loadAllTLs()
-    {
-        if (! $this->tls) {
-            foreach ($this->lotl->getTrustedListPointers('xml') as $tslPointer) {
-                try {
-                    $newTL = TrustedList::loadTrustedList($tslPointer);
-                    $this->tls[$newTL->getName()] = TrustedList::loadTrustedList($tslPointer);
-                } catch (ParseException $e) {
-                    // Tolerate unavailable/misbehaving authority
-                }
-            }
-        }
-    }
+    // public function loadAllTLs()
+    // {
+    //     if (! $this->tls) {
+    //         foreach ($this->lotl->getTrustedListPointers('xml') as $tslPointer) {
+    //             try {
+    //                 $newTL = TrustedList::loadTrustedList($tslPointer);
+    //                 $this->tls[$newTL->getName()] = TrustedList::loadTrustedList($tslPointer);
+    //             } catch (ParseException $e) {
+    //                 // Tolerate unavailable/misbehaving authority
+    //             }
+    //         }
+    //     }
+    // }
 
     public function testTrue()
     {
