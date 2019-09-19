c8ba5d1cf89724f2f810d7fb2c098bb1ee136576
Liam Dennehy <liam@wiemax.net>
2019-09-19 16:57:15 +0200

    Extension Factory

--- src/Certificate/Extension.php
+++ src/Certificate/Extension.php
@@ -16,32 +16,44 @@ abstract class Extension
     public static function fromASNObject($extension)
     {
         $extensionOid = $extension[0]->getContent();
+        if (get_class($extension[1]) == "FG\ASN1\Universal\Boolean") {
+            $isCritical = ($extension[1]->getContent() === "TRUE");
+            $extnValue = hex2bin($extension[2]->getContent());
+        } else {
+            $isCritical = false;
+            $extnValue = hex2bin($extension[1]->getContent());
+        }
         $extensionName = OID::getName($extensionOid);
+        // print $extensionName . ": " . base64_encode($extnValue) .PHP_EOL;
         switch ($extensionName) {
           case 'basicConstraints':
-            return new BasicConstraints($extension->getbinary());
-            // TODO: Properly Basic Constraints
+            // TODO: Properly handle Basic Constraints
+            return new BasicConstraints($extnValue);
             break;
           case 'preCertPoison':
-            return new PreCertPoison($extension->getbinary());
+            return new PreCertPoison($extnValue);
             // TODO: Properly handle poisoned certificates
             break;
           case 'keyUsage':
-            return new KeyUsage($extension->getbinary());
+            return false; // Canot parse bit strings with current library
+            // return new KeyUsage($extnValue);
             break;
-          // case 'authorityKeyIdentifier':
-          //   return new AuthorityKeyIdentifier($extension->getbinary());
-          //   break;
+          case 'authorityKeyIdentifier':
+            // TODO: Implement AKI
+            return new AuthorityKeyIdentifier($extnValue);
+            break;
+          case 'extKeyUsage':
+            // TODO: Implement EKU
+            return new ExtendedKeyUsage($extnValue);
+            break;
 
           default:
             if ($extension[1]->getContent() === "TRUE") {
-              throw new ExtensionException("Unrecognised Critical Extension OID '$extensionOid' ($extensionName), cannot proceed", 1);
+                throw new ExtensionException("Unrecognised Critical Extension OID '$extensionOid' ($extensionName), cannot proceed", 1);
             } else {
                 return new UnknownExtension($extension->getbinary());
             }
             break;
         }
-        // var_dump([$extension[0]->getContent(), $name]);
-        // return($extention);
     }
 }
