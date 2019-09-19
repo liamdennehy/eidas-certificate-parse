bc30c610e23d5a1cb40f34b27f0755d0b5bc826b
Liam Dennehy <liam@wiemax.net>
2019-09-19 16:41:33 +0200

    v3Extension: AuthorityKeyIdentifier

--- src/Certificate/AuthorityKeyIdentifier.php
+++ src/Certificate/AuthorityKeyIdentifier.php
@@ -10,24 +10,24 @@ use FG\ASN1\ASNObject;
  *
  */
  class AuthorityKeyIdentifier implements ExtensionInterface
-{
-    private $binary;
-    const type = 'authorityKeyIdentifier';
-    const oid = '2.5.29.35';
-    const uri = 'https://tools.ietf.org/html/rfc5280#section-4.2.1.1';
+ {
+     private $binary;
+     const type = 'authorityKeyIdentifier';
+     const oid = '2.5.29.35';
+     const uri = 'https://tools.ietf.org/html/rfc5280#section-4.2.1.1';
 
-    public function __construct($asn1Extension)
-    {
-        $this->binary = ASNObject::fromBinary($asn1Extension);
-    }
+     public function __construct($asn1Extension)
+     {
+         $this->binary = ASNObject::fromBinary($asn1Extension);
+     }
 
-    public function getType()
-    {
-        return self::type;
-    }
+     public function getType()
+     {
+         return self::type;
+     }
 
-    public function getURI()
-    {
-        return self::uri;
-    }
-}
+     public function getURI()
+     {
+         return self::uri;
+     }
+ }
