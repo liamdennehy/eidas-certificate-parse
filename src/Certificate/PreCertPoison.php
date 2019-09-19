904c676a6ff19ecb9a81385b081a2a08c0843029
Liam Dennehy <liam@wiemax.net>
2019-09-19 16:56:49 +0200

    v3Extension: PreCertPoison

--- src/Certificate/PreCertPoison.php
+++ src/Certificate/PreCertPoison.php
@@ -10,24 +10,24 @@ use FG\ASN1\ASNObject;
  *
  */
  class PreCertPoison implements ExtensionInterface
-{
-    private $binary;
-    const type = 'preCertPoison';
-    const oid = '1.3.6.1.4.1.11129.2.4.3';
-    const uri = 'https://tools.ietf.org/html/rfc6962#section-3.1';
+ {
+     private $binary;
+     const type = 'preCertPoison';
+     const oid = '1.3.6.1.4.1.11129.2.4.3';
+     const uri = 'https://tools.ietf.org/html/rfc6962#section-3.1';
 
-    public function __construct($asn1Extension)
-    {
-        $this->binary = ASNObject::fromBinary($asn1Extension);
-    }
+     public function __construct($asn1Extension)
+     {
+         $this->binary = ASNObject::fromBinary($asn1Extension);
+     }
 
-    public function getType()
-    {
-        return self::type;
-    }
+     public function getType()
+     {
+         return self::type;
+     }
 
-    public function getURI()
-    {
-        return self::uri;
-    }
-}
+     public function getURI()
+     {
+         return self::uri;
+     }
+ }
