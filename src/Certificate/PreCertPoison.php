5d262dcf6af267398c1a5214ba19a31f37e4e719
Liam Dennehy <liam@wiemax.net>
2019-09-23 15:34:53 +0200

    PreCertPoison: move to fop\asn1

--- src/Certificate/PreCertPoison.php
+++ src/Certificate/PreCertPoison.php
@@ -3,36 +3,34 @@
 namespace eIDASCertificate\Certificate;
 
 use eIDASCertificate\Certificate\ExtensionInterface;
-use eIDASCertificate\OID;
-use FG\ASN1\ASNObject;
 
 /**
  *
  */
- class PreCertPoison implements ExtensionInterface
- {
-     private $binary;
-     const type = 'preCertPoison';
-     const oid = '1.3.6.1.4.1.11129.2.4.3';
-     const uri = 'https://tools.ietf.org/html/rfc6962#section-3.1';
+class PreCertPoison implements ExtensionInterface
+{
+    private $binary;
+    const type = 'preCertPoison';
+    const oid = '1.3.6.1.4.1.11129.2.4.3';
+    const uri = 'https://tools.ietf.org/html/rfc6962#section-3.1';
 
-     public function __construct($asn1Extension)
-     {
-         $this->binary = ASNObject::fromBinary($asn1Extension);
-     }
+    public function __construct($qcStatementDER)
+    {
+        $this->binary = $qcStatementDER;
+    }
 
-     public function getType()
-     {
-         return self::type;
-     }
+    public function getType()
+    {
+        return self::type;
+    }
 
-     public function getURI()
-     {
-         return self::uri;
-     }
+    public function getURI()
+    {
+        return self::uri;
+    }
 
-     public function getBinary()
-     {
-         return $this->binary;
-     }
- }
+    public function getBinary()
+    {
+        return $this->binary;
+    }
+}
