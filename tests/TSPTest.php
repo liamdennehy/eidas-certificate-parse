2dcb14be657075eef8d58a6319dda212ae7e55b5
Liam Dennehy <liam@wiemax.net>
2019-09-26 14:42:22 +0200

    TrustServiceProvider: Test Attributes

--- tests/TSPTest.php
+++ tests/TSPTest.php
@@ -6,6 +6,7 @@ use PHPUnit\Framework\TestCase;
 use eIDASCertificate\DataSource;
 use eIDASCertificate\TrustedList;
 use eIDASCertificate\Certificate\X509Certificate;
+use eIDASCertificate\tests\LOTLRootTest;
 
 class TSPTest extends TestCase
 {
@@ -19,8 +20,9 @@ class TSPTest extends TestCase
 
     public function setUp()
     {
+        $abc = LOTLRootTest::lotlAttributes;
         $this->datadir = __DIR__ . '/../data';
-        $xmlFilePath = $this->datadir.self::lotlXMLFileName;
+        $xmlFilePath = $this->datadir.'/'.self::lotlXMLFileName;
         if (! file_exists($xmlFilePath)) {
             $lotlXML = DataSource::getHTTP(
                 TrustedList::ListOfTrustedListsXMLPath
@@ -43,8 +45,12 @@ class TSPTest extends TestCase
             0,
             sizeof($lotl->getTSPs(true))
         );
+        $crtFileName = $this->datadir.'/journal/c-276-1/d2064fdd70f6982dcc516b86d9d5c56aea939417c624b2e478c0b29de54f8474.crt';
+        $crt = file_get_contents($crtFileName);
+        $rightCert = new X509Certificate(file_get_contents($crtFileName));
+        $lotl->verifyTSL($rightCert);
         $nlFile = $this->datadir.'/tl-52f7b34b484ce888c5f1d277bcb2bfbff0b1d3bbf11217a44090fab4b6a83fd3.xml';
-        $this->lotl->addTrustedListXML("NL: Radiocommunications Agency", file_get_contents($nlFile));
+        $lotl->addTrustedListXML("NL: Radiocommunications Agency", file_get_contents($nlFile));
         $this->assertEquals(
             1,
             sizeof($lotl->getTrustedLists(false))
@@ -62,7 +68,16 @@ class TSPTest extends TestCase
             11,
             sizeof($lotl->getTSPs(true))
         );
-
+        $digidentityBV = $lotl->getTSPs(true)['Digidentity B.V.'];
+        $nlAttributes = TLTest::getNLTLAttributes();
+        $nlAttributes['ParentTSL'] = LOTLRootTest::lotlAttributes;
+        $this->assertEquals(
+            [
+            'ParentTSL' => $nlAttributes,
+            'TrustServiceProvider' => 'Digidentity B.V.'
+          ],
+            $digidentityBV->getTSPAtrributes()
+        );
         $tsps = $this->lotl->getTSPs(true);
     }
 }
