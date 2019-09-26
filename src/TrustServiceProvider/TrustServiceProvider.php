3de6156b0b51cb791f2b994d57ee1987a75edb2e
Liam Dennehy <liam@wiemax.net>
2019-09-26 14:41:16 +0200

    TrustServiceProvider: Capture parent TSL attributes

--- src/TrustServiceProvider/TrustServiceProvider.php
+++ src/TrustServiceProvider/TrustServiceProvider.php
@@ -10,16 +10,25 @@ class TrustServiceProvider
     private $name;
     private $services = [];
     private $serviceHistory;
+    private $parentTSL;
 
     /**
      * [__construct description]
      * @param SimpleXMLElement  $tsp     [description]
      */
-    public function __construct($tsp)
+    public function __construct($tsp, $trustedList)
     {
         $this->name = (string)$tsp->TSPInformation->TSPName->xpath("*[@xml:lang='en']")[0];
+        $this->parentTSL = [];
+        $this->parentTSL['SchemeTerritory'] = $trustedList->getSchemeTerritory();
+        $this->parentTSL['SchemeOperatorName'] = $trustedList->getSchemeOperatorName();
+        $this->parentTSL['TSLSequenceNumber'] = $trustedList->getSequenceNumber();
+        $this->parentTSL['TSLSignedBy'] = $trustedList->getSignedByHash();
+        if (! empty($trustedList->getParentTrustedListAtrributes())) {
+            $this->parentTSL['ParentTSL'] = $trustedList->getParentTrustedListAtrributes();
+        }
         foreach ($tsp->TSPServices->TSPService as $tspService) {
-            $newTSPService = new TSPService($tspService);
+            $newTSPService = new TSPService($tspService, $this);
             $this->services[$newTSPService->getDate()] = $newTSPService;
         };
         sort($this->services);
@@ -41,5 +50,13 @@ class TrustServiceProvider
     public function getTSPServices()
     {
         return $this->services;
+    }
+
+    public function getTSPAtrributes()
+    {
+        return [
+          'ParentTSL' => $this->parentTSL,
+          'TrustServiceProvider' => $this->getName()
+        ];
     }
 }
