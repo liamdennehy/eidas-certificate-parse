adf147bed71f33d51f4279c5e9c539441524c4fa
Liam Dennehy <liam@wiemax.net>
2019-09-28 13:20:34 +0200

    ServiceStatus: getIsActive()

--- src/TSPService/ServiceStatus.php
+++ src/TSPService/ServiceStatus.php
@@ -9,6 +9,7 @@ class ServiceStatus
 {
     private $status;
     private $uri;
+    private $isActive;
 
     /**
      * [__construct description]
@@ -20,39 +21,51 @@ class ServiceStatus
         switch ($status) {
           case 'http://uri.etsi.org/TrstSvc/TrustedList/Svcstatus/accreditationceased':
             $this->status = 'accreditationceased';
+            $this->isActive = false;
             break;
           case 'http://uri.etsi.org/TrstSvc/TrustedList/Svcstatus/accreditationrevoked':
             $this->status = 'accreditationrevoked';
+            $this->isActive = false;
             break;
           case 'http://uri.etsi.org/TrstSvc/TrustedList/Svcstatus/accredited':
             $this->status = 'accredited';
+            $this->isActive = true;
             break;
           case 'http://uri.etsi.org/TrstSvc/TrustedList/Svcstatus/deprecatedatnationallevel':
             $this->status = 'deprecatedatnationallevel';
+            $this->isActive = false;
             break;
           case 'http://uri.etsi.org/TrstSvc/TrustedList/Svcstatus/granted':
             $this->status = 'granted';
+            $this->isActive = true;
             break;
           case 'http://uri.etsi.org/TrstSvc/TrustedList/Svcstatus/recognisedatnationallevel':
             $this->status = 'recognisedatnationallevel';
+            $this->isActive = true;
             break;
           case 'http://uri.etsi.org/TrstSvc/TrustedList/Svcstatus/setbynationallaw':
             $this->status = 'setbynationallaw';
+            $this->isActive = true;
             break;
           case 'http://uri.etsi.org/TrstSvc/TrustedList/Svcstatus/supervisionceased':
             $this->status = 'supervisionceased';
+            $this->isActive = false;
             break;
           case 'http://uri.etsi.org/TrstSvc/TrustedList/Svcstatus/supervisionincessation':
             $this->status = 'supervisionincessation';
+            $this->isActive = true;
             break;
           case 'http://uri.etsi.org/TrstSvc/TrustedList/Svcstatus/supervisionrevoked':
             $this->status = 'supervisionrevoked';
+            $this->isActive = false;
             break;
           case 'http://uri.etsi.org/TrstSvc/TrustedList/Svcstatus/undersupervision':
             $this->status = 'undersupervision';
+            $this->isActive = true;
             break;
           case 'http://uri.etsi.org/TrstSvc/TrustedList/Svcstatus/withdrawn':
             $this->status = 'withdrawn';
+            $this->isActive = false;
             break;
           default:
             $this->uri = null;
@@ -64,6 +77,11 @@ class ServiceStatus
     public function getStatus()
     {
         return $this->status;
+    }
+
+    public function getIsActive()
+    {
+        return $this->isActive;
     }
 
     public function getURI()
