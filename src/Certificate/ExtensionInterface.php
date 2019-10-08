18a164993dfd499d91bfbc514443f930f60db0ab
Liam Dennehy <liam@wiemax.net>
2019-10-08 13:02:51 +0200

    ExtensionInterface: getAttributes()
    Output will be incorporated into the parent certificate's
    attributes collection

--- src/Certificate/ExtensionInterface.php
+++ src/Certificate/ExtensionInterface.php
@@ -7,10 +7,12 @@ namespace eIDASCertificate\Certificate;
  */
  interface ExtensionInterface extends ParseInterface
  {
-     public function __construct($extensionDER, $isCritical);
+     public function __construct($extensionDER, bool $isCritical);
      public function getType();
      public function getDescription();
      public function getURI();
      public function getBinary();
      public function getIsCritical();
+     // public function setCertificate();
+     public function getAttributes();
  }
