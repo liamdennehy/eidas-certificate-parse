26ee78f4ee25e26eb89c12abde99ad6ae4f8bfcd
Liam Dennehy <liam@wiemax.net>
2019-10-16 17:57:14 +0200

    Address: Parse all types

--- src/Address.php
+++ src/Address.php
@@ -8,33 +8,61 @@ namespace eIDASCertificate;
 class Address
 {
     private $postalAddresses = [];
+    private $electronicAddresses = [];
 
     public function __construct($addressesCollection)
     {
         foreach ($addressesCollection->children() as $type => $addressTypeCollection) {
-            // var_dump($addressTypeCollection->asXML());
-            switch ($type) {
+          switch ($type) {
             case 'PostalAddresses':
-              // var_dump($addressTypeCollection->xpath('*[@xml:lang]/@xml:lang'));
               foreach ($addressTypeCollection as $postalAddress) {
-                  $lang = (string)$postalAddress->attributes('xml', true)['lang'];
-                  if (array_key_exists($lang, $this->postalAddresses)) {
-                      throw new ParseException("Multiple PostalAddresses in Language '$lang'", 1);
-                  }
-                  $thispostalAddress = [];
-                  foreach ($postalAddress as $key => $value) {
-                      $thispostalAddress[$key] = (string)$value;
-                  }
-                  $this->postalAddresses[$lang] = $thispostalAddress;
+                $lang = (string)$postalAddress->attributes('xml',true)['lang'];
+                if (array_key_exists($lang,$this->postalAddresses)) {
+                  throw new ParseException("Multiple PostalAddresses in Language '$lang'", 1);
+
+                }
+                $thispostalAddress = [];
+                foreach ($postalAddress as $key => $value) {
+                  $thispostalAddress[$key] = (string)$value;
+                }
+                $this->postalAddresses[$lang] = $thispostalAddress;
+              }
+              break;
+
+            case 'ElectronicAddress':
+              foreach ($addressTypeCollection as $type => $electronicAddress) {
+                switch ($type) {
+                  case 'URI':
+                    $lang = (string)$electronicAddress->attributes('xml',true)['lang'];
+                    $this->electronicAddresses[] = [
+                      'lang' => $lang,
+                      'uri' => (string)$electronicAddress
+                    ];
+                    break;
+
+                  default:
+                    throw new ParseException("Unrecognised ElectronicAddress element '$type'", 1);
+
+                    break;
+                }
               }
               break;
 
             default:
-              // throw new ParseException("Unrecognised Address type '$type'", 1);
+              throw new ParseException("Unrecognised Address type '$type'", 1);
 
               break;
           }
         }
-        // var_dump($this->postalAddresses);
+    }
+
+    public function getPostalAddresses()
+    {
+        return $this->postalAddresses;
+    }
+
+    public function getElectronicAddresses()
+    {
+        return $this->electronicAddresses;
     }
 }
