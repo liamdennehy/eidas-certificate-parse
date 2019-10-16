1dd6bcc789a4262990aa5da1b054cc7c632b2dec
Liam Dennehy <liam@wiemax.net>
2019-10-16 17:06:52 +0200

    TrustServiceProvider: Parse Address

--- src/Address.php
+++ src/Address.php
@@ -7,10 +7,29 @@ namespace eIDASCertificate;
  */
 class Address
 {
+    private $postalAddresses = [];
 
-    public function __construct($address)
+    public function __construct($addressesCollection)
     {
+        foreach ($addressesCollection->children() as $type => $addressTypeCollection) {
+          switch ($type) {
+            case 'PostalAddresses':
+            // var_dump($addressTypeCollection->attributes());
+              foreach ($addressTypeCollection as $postalAddress) {
+                // $postalAddress = [];
+                foreach ($postalAddress as $key => $value) {
+                  $thispostalAddress[$key] = (string)$value;
+                }
+                // var_dump($thispostalAddress);
+              }
+              break;
 
+            default:
+              // throw new ParseException("Unrecognised Address type '$type'", 1);
+
+              break;
+          }
+        }
     }
 
 }
