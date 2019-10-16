6d71d27e3107c4a1d9617d036972b5c3eaa04eaa
Liam Dennehy <liam@wiemax.net>
2019-10-16 15:07:52 +0200

    TSPService: Qualifications

---
+++ src/TSPService/Qualifications.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace eIDASCertificate\TSPService;
+
+/**
+ *
+ */
+class Qualifications
+{
+    private $qualifierURIs = [];
+
+    /**
+     * [__construct description]
+     * @param SimpleXMLElement $history [description]
+     */
+    public function __construct($qualifications)
+    {
+        if (sizeof($qualifications) > 1) {
+            throw new ParseException("Multiple TSPService Qualifications", 1);
+        }
+        foreach ($qualifications[0]->xpath('ns5:QualificationElement') as $qualificationElement) {
+            foreach ($qualificationElement->xpath('ns5:Qualifiers') as $qualifiers) {
+                foreach ($qualifiers->xpath('ns5:Qualifier') as $qualifier) {
+                  $this->qualifierURIs[] = (string)$qualifier->attributes()['uri'];
+                }
+            }
+            foreach ($qualificationElement->xpath('ns5:CriteriaList') as $criteriaList) {
+                // code...
+            }
+        }
+    }
+
+    public function getQualifierURIs()
+    {
+        return $this->qualifierURIs;
+    }
+
+    public function getInstances()
+    {
+        return $this->historyInstances;
+    }
+}
