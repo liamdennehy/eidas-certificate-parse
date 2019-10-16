557ef7247ee7c86ca1cea3a4d565c17e17c357f0
Liam Dennehy <liam@wiemax.net>
2019-10-16 18:57:45 +0200

    Address: cs

--- src/Address.php
+++ src/Address.php
@@ -13,27 +13,26 @@ class Address
     public function __construct($addressesCollection)
     {
         foreach ($addressesCollection->children() as $type => $addressTypeCollection) {
-          switch ($type) {
+            switch ($type) {
             case 'PostalAddresses':
               foreach ($addressTypeCollection as $postalAddress) {
-                $lang = (string)$postalAddress->attributes('xml',true)['lang'];
-                if (array_key_exists($lang,$this->postalAddresses)) {
-                  throw new ParseException("Multiple PostalAddresses in Language '$lang'", 1);
-
-                }
-                $thispostalAddress = [];
-                foreach ($postalAddress as $key => $value) {
-                  $thispostalAddress[$key] = (string)$value;
-                }
-                $this->postalAddresses[$lang] = $thispostalAddress;
+                  $lang = (string)$postalAddress->attributes('xml', true)['lang'];
+                  if (array_key_exists($lang, $this->postalAddresses)) {
+                      throw new ParseException("Multiple PostalAddresses in Language '$lang'", 1);
+                  }
+                  $thispostalAddress = [];
+                  foreach ($postalAddress as $key => $value) {
+                      $thispostalAddress[$key] = (string)$value;
+                  }
+                  $this->postalAddresses[$lang] = $thispostalAddress;
               }
               break;
 
             case 'ElectronicAddress':
               foreach ($addressTypeCollection as $type => $electronicAddress) {
-                switch ($type) {
+                  switch ($type) {
                   case 'URI':
-                    $lang = (string)$electronicAddress->attributes('xml',true)['lang'];
+                    $lang = (string)$electronicAddress->attributes('xml', true)['lang'];
                     $this->electronicAddresses[] = [
                       'lang' => $lang,
                       'uri' => (string)$electronicAddress
