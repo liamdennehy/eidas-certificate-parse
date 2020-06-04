5fd1dd12513f58e49ad017bc4f80e60e04cfe4fd
Liam Dennehy <liam@wiemax.net>
2020-06-04 16:14:34 +0200

    ResponseData: getAttributes()

--- src/OCSP/ResponseData.php
+++ src/OCSP/ResponseData.php
@@ -3,14 +3,18 @@
 namespace eIDASCertificate\OCSP;
 
 use ASN1\Type\UnspecifiedType;
-// use eIDASCertificate\ASN1Interface;
-// use eIDASCertificate\AttributeInterface;
+use eIDASCertificate\ASN1Interface;
+use eIDASCertificate\AttributeInterface;
 use eIDASCertificate\DistinguishedName;
 use eIDASCertificate\Extensions;
 use ASN1\Type\Constructed\Sequence;
+use ASN1\Type\Tagged\ImplicitlyTaggedType;
+use ASN1\Type\Tagged\ExplicitlyTaggedType;
+use ASN1\Type\Primitive\GeneralizedTime;
+use ASN1\Type\Primitive\Integer;
+use ASN1\Type\Primitive\OctetString;
 
-// TODO: implements ASN1Interface, AttributeInterface
-class ResponseData
+class ResponseData implements ASN1Interface, AttributeInterface
 {
     private $version;
     private $responderId;
@@ -19,23 +23,26 @@ class ResponseData
     private $extensions;
 
     public function __construct(
-        $version, $responderId, $producedAt, $singleResponses, $extensions = null
+        $version,
+        $responderId,
+        $producedAt,
+        $singleResponses,
+        $extensions = null
     ) {
-      $version = $version;
-      $responderId = $responderId;
-      $producedAt = $producedAt;
-      $singleResponses = $singleResponses;
-      $extensions = $extensions;
+        $this->version = $version;
+        $this->responderId = $responderId;
+        $this->producedAt = $producedAt;
+        $this->singleResponses = $singleResponses;
+        $this->extensions = $extensions;
     }
 
-    public function fromDER($der)
+    public static function fromDER($der)
     {
-        self::fromSequence(UnspecifiedType::fromDER($der)->asSequence());
+        return self::fromSequence(UnspecifiedType::fromDER($der)->asSequence());
     }
 
     public static function fromSequence($tbsResponseData)
     {
-
         $idx = 0;
         if ($tbsResponseData->at($idx)->tag() == 0) {
             $version = $tbsResponseData->at($idx)->asInteger()->integer();
@@ -43,16 +50,21 @@ class ResponseData
             $idx++;
             throw new \Exception("ResponData version $version not supported", 1);
         } else {
-          $version = 1;
+            $version = 1;
         }
         $responderId = $tbsResponseData->at($idx++);
         switch ($responderId->tag()) {
           case 1:
             $responderId = new DistinguishedName($responderId->asImplicit(16)->at(0));
             break;
-          case 1:
-            $responderId = new DistinguishedName($responderId->asImplicit(4)->string());
+          case 2:
+            $responderId = $responderId->asImplicit(4)->string();
             break;
+          default:
+            throw new \Exception("Unkown RespondeID tag ".$responderId->tag(), 1);
+
+            break;
+
         }
         $producedAt = $tbsResponseData->at($idx++)->asGeneralizedTime()->dateTime();
         $responses = $tbsResponseData->at($idx++)->asSequence();
@@ -66,11 +78,50 @@ class ResponseData
             $extensions = null;
         }
         return new ResponseData(
-          $version,
-          $responderId,
-          $producedAt,
-          $singleResponses,
-          $extensions
+            $version,
+            $responderId,
+            $producedAt,
+            $singleResponses,
+            $extensions
         );
+    }
+
+    public function getBinary()
+    {
+        return $this->getASN1()->toDER();
+    }
+
+    public function getASN1($value='')
+    {
+        $asn1 = new Sequence();
+        if ($this->version <> 1) {
+            $asn1 = $asn1->withAppended(new ExplicitlyTaggedType(0, new Integer($this->version - 1)));
+        };
+        if (is_string($this->responderId)) {
+            $asn1 = $asn1->withAppended(new ImplicitlyTaggedType(2, new Sequence(new OctetString($this->responderId))));
+        } else {
+            $asn1 = $asn1->withAppended(new ImplicitlyTaggedType(1, $this->responderId->getASN1()));
+        }
+        $asn1 = $asn1->withAppended(new GeneralizedTime($this->producedAt));
+        foreach ($this->singleResponses as $response) {
+            $responses[] = $response->getASN1();
+        }
+        $asn1 = $asn1->withAppended(new Sequence(...$responses));
+        if (! empty($this->extensions)) {
+          $asn1 = $asn1->withAppended(new ExplicitlyTaggedType(1, $this->extensions->getASN1()));
+        }
+        return $asn1;
+    }
+
+    public function getAttributes()
+    {
+        $attr['producedAt'] = intval($this->producedAt->format('U'));
+        foreach ($this->singleResponses as $response) {
+            $attr['responses'][] = $response->getAttributes();
+        }
+        if (! empty($this->extensions) && array_key_exists('ocspNonce', $this->extensions->getExtensions())) {
+            $attr['nonce'] = bin2hex($this->extensions->getExtensions()['ocspNonce']->getAttributes()['nonce']);
+        }
+        return $attr;
     }
 }
