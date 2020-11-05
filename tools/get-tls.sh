331d4dd5a4f95dd88870c1e101cb35c865496812
Liam Dennehy <liam@wiemax.net>
2020-11-05 11:40:16 +0100

    Silence curl progress bars

--- tools/get-tls.sh
+++ tools/get-tls.sh
@@ -20,7 +20,7 @@ php ./tools/get-tl-urls.php | jq -c '.[]' | while read line; do
   name="$(echo $line | jq .name -r)";
   filename=$(echo $line | jq .filename -r);
   echo $name;
-  curl -k "$(echo $line | jq -r .url)" -A 'eIDAS PHP Certificate Library' > data/tl/$filename;
+  curl -sk "$(echo $line | jq -r .url)" -A 'eIDAS PHP Certificate Library' > data/tl/$filename;
   done
 
 date +%s > data/tl/lastupdate
